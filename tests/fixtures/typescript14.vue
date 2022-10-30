91fe4d47f70b042bfa816192297a930db5bc139a
liulinboyi <814921718@qq.com>
2022-10-30 12:05:15 +0800

    feat: add more test case

---
+++ tests/fixtures/typescript14.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import { Foo } from './index'
+defineProps<
+
+
+Foo
+
+
+
+
+>()
+console.log('Hello')
+</script>
+
+<template>
+  <div>
+    <h1>Hello World</h1>
+  </div>
+</template>
